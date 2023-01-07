1957d3a64988c0432d9246a39599970ae9fb5bd1
rthota7 <rthota7@dxc.com>
2023-01-07 17:28:53 +0530

    adding project

---
+++ src/test/java/com/springboot/Author/service/AuthorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Author.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
